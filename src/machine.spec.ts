31cf9bbfb2f0cffa9b806462b2b92c40d52ce168
Krzysztof Żuraw <services@kzuraw.com>
2020-04-21 17:24:55 +0200

    test: basic machine tests

--- src/machine.spec.ts
+++ src/machine.spec.ts
@@ -1,42 +1,42 @@
 import { interpret } from 'xstate';
 
 import { autocompleteMachine, Context } from './machine';
+import { GeocodeResult } from './models';
+
+const mockedRespnse: GeocodeResult = {
+  text: 'henryka',
+  id: '42',
+  zipCode: '50-043',
+  place: 'Henryka',
+  region: 'Dolnoslaskie',
+  country: 'PL',
+  center: [52, 17],
+  type: ['address'],
+};
 
 const mockFetchGeocodingService = async (_context: Context, _service: {}) => {
-  return {
-    text: 'OMG',
-  };
+  return [mockedRespnse];
 };
 
 describe('autocompleteMachine', () => {
-  // it('integration tests', done => {
-  //   const autocompleteService = interpret(autocompleteMachine)
-  //     .onTransition(state => {
-  //       if (state.matches({ submitted: 'loaded' })) {
-  //         expect(state.context.results).not.toEqual([]);
-  //         // expect(state.context.query).toEqual('Henryka Brodatego 4, Wrocław');
-  //         done();
-  //       }
-  //     })
-  //     .start();
-  //   autocompleteService.send('SUBMIT', { query: 'Henryka Brodatego 4, Wrocław' });
-  // });
-
   const autocompleteTestMachine = autocompleteMachine.withConfig({
     services: {
       geocodeQuery: context => mockFetchGeocodingService(context, {}),
     },
   });
 
-  it('should work', () => {
+  it('should send request to geocode when user stoped typing', () => {
     const autocompleteService = interpret(autocompleteTestMachine)
       .onTransition(state => {
         if (state.matches('loaded')) {
-          expect(state.context.results).toEqual([]);
+          expect(state.context.results).toEqual([mockedRespnse]);
         }
       })
       .start();
 
-    autocompleteService.send('KEYSTROKE', { query: 'SUPER' });
+    autocompleteService.send('KEYSTROKE', { query: 'h' });
+    autocompleteService.send('KEYSTROKE', { query: 'e' });
+    autocompleteService.send('KEYSTROKE', { query: 'n' });
+    autocompleteService.send('STOPED');
   });
 });
